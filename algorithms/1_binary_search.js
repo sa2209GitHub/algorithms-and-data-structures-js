f2346b670c864e6f032e0d8ab89212e54c680b18
Sergey Arkhangelskiy <sergey.arkhangelskiy@gmail.com>
2023-01-06 17:13:20 +0300

    Added binary search implementations

---
+++ algorithms/1_binary_search.js
@@ -0,0 +1,37 @@
+/**
+ *      Binary Search
+ *      Iterative and Recursive Solutions
+ */
+
+const binarySearchIterative = (array, value) => {
+    let low = 0
+    let high = array.length - 1
+    
+    while (low <= high) {
+        let mid = Math.round((low + high) / 2)
+        
+        switch (true) {
+            case value > array[mid]: low = mid + 1  ;   break
+            case value < array[mid]: high = mid - 1 ;   break
+            default: return mid
+        }
+    }
+    
+    return `${value}: not found`
+}
+
+const binarySearchRecursive = (array, value, low = 0, high = array.length) => {
+    const mid = Math.round((low + high) / 2)
+
+    switch (true) {
+        case low > high:            return `${value}: not found`
+        case value > array[mid]:    return binarySearchRecursive(array, value, mid + 1, high)
+        case value < array[mid]:    return binarySearchRecursive(array, value, low, mid - 1)
+        default:                    return mid
+    }
+}
+
+module.exports = {
+    binarySearchIterative,
+    binarySearchRecursive
+}
---
+++ specs/algorithms/1_binary_search.spec.js
@@ -0,0 +1,28 @@
+/**
+ *      Binary Search:
+ *      Iterative and Recursive solutions
+ */
+
+const chai = require('chai')
+const assert = chai.assert
+const { binarySearchIterative, binarySearchRecursive } = require('../../algorithms/1_binary_search')
+
+describe ('Binary search', () => {
+    const array = []
+    for (let i = 98; i < 99999; i++) array.push(i)
+
+    describe ('Iterative fubction', () => {
+        it ('Should return correct Array index or \'not found\' message', () => {
+            assert.equal(binarySearchIterative(array, 10), '10: not found')
+            assert.equal(binarySearchIterative(array, 101), 3)
+            assert.equal(binarySearchIterative(array, 5555), 5457)
+        })
+    })
+    describe('Recursive function', () => {
+        it ('Should return correct Array index or \'not found\' message', () => {
+            assert.equal(binarySearchRecursive(array, 10), '10: not found')
+            assert.equal(binarySearchRecursive(array, 101), 3)
+            assert.equal(binarySearchRecursive(array, 5555), 5457)
+        })
+    })
+})
